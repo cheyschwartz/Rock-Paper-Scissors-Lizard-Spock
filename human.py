89f2a2d9f3199128d9dee704fbbdeb4a86bb8cd4
unknown <cheyenne.schwartz07@gmail.com>
2022-06-05 17:42:42 -0500

    Changed the chosen gesture logic to run as a number and not a string

--- human.py
+++ human.py
@@ -6,23 +6,9 @@ class Human(Player):
         self.name = "Player One"
         super().__init__(self.name)
         self.score = 0
-        
-
 
     def choose_gesture(self):
-        self.chosen_gesture = input("Press 0 for Rock, 1 for Paper, 2 for Scissors, 3 Lizard, 4 Spock")
-        print(f"{self.name} has picked {self.gesture_list[int(self.chosen_gesture)]}")
+        self.chosen_gesture = int(input("Press 0 for Rock, 1 for Paper, 2 for Scissors, 3 Lizard, 4 Spock"))
+        print(f"{self.name} has picked {self.gesture_list[self.chosen_gesture]}")
 
 
-    # def human_choice():
-    #     choice = [0,4]
-    #     if choice == 0:
-    #         print("Player picked Rock")
-    #     elif choice == 1:
-    #         print("Player picked Paper")
-    #     elif choice == 2:
-    #         print("Player picked Scissors")
-    #     elif choice == 3:
-    #         print("Player picked Lizard")
-    #     elif choice == 4:
-    #         print("Player picked Spock")
